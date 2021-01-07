ded730226d7c1974de08c9dbb505dc4af19563af
Goran <goranfrance@hotmail.com>
2021-01-07 12:33:26 +0000

    a;fjda

--- exercises.js
+++ exercises.js
@@ -1,17 +1,31 @@
-function reverseString(str) {
-  return str.split('').reverse().join('');
-// let revString = "";
-// for(let i = str.length - 1; i >= 0; i--){
-//    revString = revString + str[i];
+// CHALLANGES
+// CHALLANGE1 
+// function reverseString(str) {
+//   return str.split('').reverse().join('');
+// // let revString = "";
+// // for(let i = str.length - 1; i >= 0; i--){
+// //    revString = revString + str[i];
+// // }
+// // return revString;
 // }
-// return revString;
-}
 
 
-const output = reverseString('Hello');
+// const output = reverseString('Hello');
 
-console.log(output);
+// console.log(output);
 
+// CHALLANGE 2 
+function isPalindrome(str) {
+  const reverseStr = str.split('').reverse().join('');
+
+  return reverseStr === str;
+  }
+  
+
+  const output = isPalindrome('racecar');
+  
+  console.log(output);
+  
 
 
 
