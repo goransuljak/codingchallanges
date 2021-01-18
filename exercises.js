97f6ba5a715504aad4c51405453fa2008386cd86
Goran <goranfrance@hotmail.com>
2021-01-18 15:52:41 +0000

    exercise

--- exercises.js
+++ exercises.js
@@ -1,16 +1,37 @@
 const openButtons = document.querySelectorAll("[data-modal-target]");
 const closeButtons = document.querySelectorAll("[data-close-btn]");
+const overlay = document.getElementById("overlay");
 
 
 
+openButtons.forEach(button => {
+    button.addEventListener('click', ()=> {
+     const modal = document.querySelector(button.dataset.modalTarget);
+     openModal(modal);
+    })
+})
+closeButtons.forEach(button => {
+    button.addEventListener('click', ()=> {
+     const modal = button.closest('.modal')
+     closeModal(modal);
+    })
+})
 
 
 
+function openModal(modal) {
+ if(modal == null) return
+ modal.classList.add('active')
+ overlay.classList.add('active')
+}
 
 
-
-
-
+function closeModal(modal) {
+    if(modal == null) return
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
+   }
+   
 
 
 
