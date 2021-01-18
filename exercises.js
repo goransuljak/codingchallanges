80665684ebc566f8485f11c568c19bebbc66021e
Goran <goranfrance@hotmail.com>
2021-01-18 12:47:50 +0000

    o

--- exercises.js
+++ exercises.js
@@ -15,32 +15,93 @@
 // console.log(output);
 
 // CHALLANGE 2 
-function isPalindrome(str) {
-  const reverseStr = str.split('').reverse().join('');
+// function isPalindrome(str) {
+//   const reverseStr = str.split('').reverse().join('');
 
-  return reverseStr === str;
-  }
+//   return reverseStr === str;
+//   }
   
 
-  const output = isPalindrome('racecar');
+//   const output = isPalindrome('racecar');
   
-  console.log(output);
+//   console.log(output);
   
+// CHALLANGE 3
+
+// function reverseNum(int) {
+//  const revNum = int.toString().split('').reverse().join('');
+
+//  return parseInt(revNum) * Math.sign(int);
+// }
+      
+    
+// const output = reverseNum(521);
+
+// console.log(output);
+
+
+// CHALLENGE 4
+
+// function capitalizeLetter(str) {
+// // const capLetter = str.toLowerCase().split(' ');
+
+// // for(let i = 0; i < capLetter.length; i++) {
+// //   capLetter[i] = capLetter[i].substring(0, 1).toUpperCase() + 
+// //   capLetter[i].substring(1).toLowerCase();
+// // }
+// // return capLetter.join(' ');
+// return str.toLowerCase().split(' ')
+// .map(char => {
+//     return char[0].toUpperCase() + char.substring(1);
+// }).join(' ')
+// }     
+       
+// const output = capitalizeLetter("i love javascript");
+   
+// console.log(output);
+
+// CHALLENGE 5
+
+// function maxCharacter(str) {
+//  const charMap = {};
+//  let maxNum = 0;
+//  let maxChar = '';
+
+//  str.split('').forEach(function(char){
+//      if(charMap[char]){
+//          charMap[char]++;
+//      } else {
+//          charMap[char] = 1;
+//      }
+//  })
+// for(let char in charMap) {
+// if(charMap[char] > maxNum){
+//   maxNum = charMap[char];
+//   maxChar =  char;
+// }
+// }
+
+// return maxNum;
+// }
+         
+       
+// const output = maxCharacter('blalblabla');
+   
+// console.log(output);
 
 
 
 
 
 
-
-
-
-
-
-
-
-
-
+function fizzBuzz() {
+}
+            
+          
+const output = fizzBuzz();
+      
+console.log(output);
+   
 
 
 
