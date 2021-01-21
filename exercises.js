24833f0eeae9e46b2280d14e68d85f9f22ca691f
Goran <goranfrance@hotmail.com>
2021-01-21 21:34:17 +0000

    fda

---
+++ exercises.js
@@ -0,0 +1,32 @@
+const button = document.getElementById("button");
+const toasts = document.getElementById("toasts");
+
+
+const messages = [
+    "Got new like",
+    "Visited by Mary",
+    "Welcome back",
+    "Subscribe",
+]
+    
+
+
+
+button.addEventListener("click", () => createNotification());
+
+function createNotification() {
+    const notif = document.createElement("div");
+    notif.classList.add("toast");
+
+
+    notif.innerText = getRandomMessage();
+    toasts.appendChild(notif);
+
+    setTimeout(() => {
+        notif.remove()
+    }, 3000)
+}
+
+function getRandomMessage() {
+    return messages[Math.floor(Math.random() * messages.length)];
+}
