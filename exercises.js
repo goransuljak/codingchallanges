d4c24f92aa6de04cf3f38488339313266eec4f4b
Goran <goranfrance@hotmail.com>
2021-01-22 10:32:23 +0000

    asf

--- exercises.js
+++ exercises.js
@@ -26,19 +26,29 @@ function showMovies(movies) {
 
         movieEl.classList.add("movie");
         movieEl.innerHTML = `
-        <div class="movie">
-        <img src="" alt="">
+      
+        <img src="${IMG_PATH + poster_path}" alt="${title}">
         <div class="movie-info">
-            <h3>Movie Title</h3>
-            <span class="green">9.8</span>
+            <h3>${title}</h3>
+            <span class="${getClassByRate(vote_average)}">${vote_average}</span>
         </div>
         <div class="overview">
             <h3>Overview</h3>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias totam illo sequi repellat nam? Sequi animi iure doloremque laborum distinctio.
+           ${overview}
         </div>
-    </div>
+  
         `
+        main.appendChild(movieEl)
     })
+}
+function getClassByRate(vote) {
+ if(vote >= 8) {
+     return "green";
+ } else if(vote >= 5){
+     return "orange"
+ } else {
+     return "red"
+ }
 }
 
 form.addEventListener("submit", (e) => {
