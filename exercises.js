10820610ee2de4e1ebaf706df7f521fead8d8424
Goran <goranfrance@hotmail.com>
2021-01-27 11:04:16 +0000

    lol

---
+++ exercises.js
@@ -0,0 +1,18 @@
+const poke_container = document.getElementById("poke-container");
+const pokemon_count = 150;
+const colors = {
+    fire: '#FDDFDF',
+    grass: '#DEFDE0',
+    electric: '#fcf7de',
+    water: '#def3fd',
+    ground: '#f4e7da',
+    rock: '#d5d5d4',
+    fairy: '#fceaff',
+    poison: '#98d7a5',
+    bug: '#f8d5a3',
+    dragon: '#97b3e6',
+    psyhic: '#eaeda1',
+    flying: '#f5f5f5',
+    fighting: '#e6e0d4',
+    normal: '#f5f5f5'
+}
